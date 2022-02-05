627ac2c3878ff614d2d9ff4d322910eca5c50a53
Gustavo Maia <67283753+gustavopmaia@users.noreply.github.com>
2022-02-04 21:36:59 -0300

    chore: Add condition
    Condition to return 0 when Delta is negative

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@ module Bhaskara where
 bhaskara :: Floating a => a -> a -> a -> (a, a)
 bhaskara a b c = (x1, x2)
   where
-    x1 = ((-b) + sqDelta) / (2 * a)
-    x2 = ((-b) - sqDelta) / (2 * a)
+    x1 = if sqDelta >= 0 then ((-b) - sqDelta) / (2 * a)
+    x2 = if sqDelta >= 0 then ((-b) - sqDelta) / (2 * a)
     sqDelta = sqrt delta
     delta = b^2 - 4 * a * c
